657dd37eb162d01b9bb49a8f9b2aabbef066f3ca
lalinthorn <lalinpye@gmail.com>
2021-07-25 21:28:11 +0700

    no local storage

--- script.js
+++ script.js
@@ -4,51 +4,18 @@ const Main = document.createElement('div')
 const Todo = document.createElement('div')
 const Mark = document.createElement('div')
 const bt = document.querySelector('button')
-input.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        if (input.value == "") alert("Task cannot be empty")
-        else {
-            const Task = document.createElement('div')
-            const text = document.createElement('span')
-            text.innerHTML = input.value
-    
-            const Delbt = document.createElement('button')
-            Delbt.innerText = 'Delete'
-            Delbt.style.visibility = "hidden"
-            Delbt.addEventListener('click', () => {
-                Task.remove()
-            })
-            Task.addEventListener('mouseout', () => { Delbt.style.visibility = "hidden" })
-            Task.addEventListener('mouseover', () => { Delbt.style.visibility = "visible" })
-    
-            const Donebt = document.createElement('button')
-            Donebt.innerText = 'Done'
-            Donebt.style.visibility = "hidden"
-            Donebt.addEventListener('click', () => {
-                const ntext = document.createElement('span')
-                ntext.innerHTML = text.innerHTML + " <br> "
-                ntext.style.textDecoration = "line-through"
-                Todo.removeChild(Task)
-                Mark.prepend(ntext)
-            })
-            Task.addEventListener('mouseout', () => { Donebt.style.visibility = "hidden" })
-            Task.addEventListener('mouseover', () => { Donebt.style.visibility = "visible" })
-    
-            Task.append(text)
-            Task.append(Donebt)
-            Task.append(Delbt)
-            Todo.prepend(Task)
-        }   
-    }
-})
-bt.addEventListener('click', () => {
+
+function Add(){
     if (input.value == "") alert("Task cannot be empty")
     else {
         const Task = document.createElement('div')
-        const text = document.createElement('span')
+        Task.classList.add('Text','flex')
+        const text = document.createElement('div')
         text.innerHTML = input.value
+        input.value=''
 
         const Delbt = document.createElement('button')
+        Delbt.classList.add('delbtn')
         Delbt.innerText = 'Delete'
         Delbt.style.visibility = "hidden"
         Delbt.addEventListener('click', () => {
@@ -58,14 +25,17 @@ bt.addEventListener('click', () => {
         Task.addEventListener('mouseover', () => { Delbt.style.visibility = "visible" })
 
         const Donebt = document.createElement('button')
+        Donebt.classList.add('donebtn')
         Donebt.innerText = 'Done'
         Donebt.style.visibility = "hidden"
         Donebt.addEventListener('click', () => {
-            const ntext = document.createElement('span')
+            const ntext = document.createElement('div')
             ntext.innerHTML = text.innerHTML + " <br> "
             ntext.style.textDecoration = "line-through"
             Todo.removeChild(Task)
             Mark.prepend(ntext)
+            Mark.classList.add('nText',)
+            
         })
         Task.addEventListener('mouseout', () => { Donebt.style.visibility = "hidden" })
         Task.addEventListener('mouseover', () => { Donebt.style.visibility = "visible" })
@@ -74,8 +44,14 @@ bt.addEventListener('click', () => {
         Task.append(Donebt)
         Task.append(Delbt)
         Todo.prepend(Task)
-    }
+    }   
+}
+
+input.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') Add()
 })
+
+bt.addEventListener('click', () => Add())
 
 Main.append(Todo)
 Main.append(Mark)
